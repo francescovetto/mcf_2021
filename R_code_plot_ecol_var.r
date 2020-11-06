d0e821a571b6286de412fd7b94134a0f5dd9d320
francescovetto <73694014+francescovetto@users.noreply.github.com>
2020-11-06 10:30:27 +0100

    Create R_code_plot_ecol_var.r

---
+++ R_code_plot_ecol_var.r
@@ -0,0 +1,26 @@
+# here you can write everything! this is a comment
+
+# R code for plotting the relationship among ecoogical variables
+
+# install.packages is used to install packages
+install.packages("sp")
+library(sp) 
+
+# without breakets beacuse we are already into R
+
+# data is used to recall datsets
+data(meuse)
+
+# look inside the set
+meuse
+
+# View(meuse) doesn't work for mac
+solution is to install https://www.xquartz.org
+
+head(meuse)
+
+# exercise: mean of all of the variables
+
+
+
+
