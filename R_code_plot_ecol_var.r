d5847eb4bba9926ae3514be93013232b048a7076
francescovetto <73694014+francescovetto@users.noreply.github.com>
2020-11-06 11:06:31 +0100

    Update R_code_plot_ecol_var.r

--- R_code_plot_ecol_var.r
+++ R_code_plot_ecol_var.r
@@ -20,7 +20,26 @@ solution is to install https://www.xquartz.org
 head(meuse)
 
 # exercise: mean of all of the variables
+summary(meuse)
 
+# exercise: plot zinc (y) against cadmium (x)
+# error
+plot(cadmium, zinc)
+# Error in h(simpleError(msg, call)) : error in evaluating the argument 'x' in selecting a method for function 'plot': object 'cadmium' not found
 
+# $ used to link in R different parts
+plot(meuse$cadmium,meuse$zinc)
 
+attach(meuse) 
+# from now on we don't need to use the $
+plot(cadmium,zinc)
 
+plot(cadmium,lead)
+
+# how to impress your supervisor
+# scatterplot matrices
+pairs(meuse)
+
+# question 
+# pairing only the elements part of the dataset: how to do that?
+# only with cadmium, copper, lead, zinc..
