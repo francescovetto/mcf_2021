f792931c9fe862bc6e21b930bc7575a7696be4ad
francescovetto <73694014+francescovetto@users.noreply.github.com>
2020-12-18 10:35:49 +0100

    Update R_code_multivar.r

--- R_code_multivar.r
+++ R_code_multivar.r
@@ -1,4 +1,15 @@
 install.packages("vegan")
 library(vegan)
 
-load
+load("biomes_multivar.RData")
+
+ls()
+
+# plot for species matrix
+
+head(biomes)
+
+# detrended correspondence analysis, see how this species are related to each other
+# species that are living together, in the same are are closed in the plot 
+multivar <- decorana(biomes)
+multivar
