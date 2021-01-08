0260dc545f131e2c80365921f96791646cf715d1
francescovetto <73694014+francescovetto@users.noreply.github.com>
2021-01-08 10:55:32 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -16,11 +16,21 @@ clb <- colorRampPalette(c('dark blue','blue','light blue'))(100)
 plot(p224r63_2011$B1_sre, col=clb)
 # plot just one image, the $ to attach the right one
 
-# do the same for the others vents
+# do the same for the others bands
 
 clg <- colorRampPalette(c('dark green','green','light green'))(100)
 plot(p224r63_2011$B2_sre, col=clg)
 
-clr <- colorRampPalette(c('dark red','red','light red'))(100)
+clr <- colorRampPalette(c('dark red','red','orange'))(100)
 plot(p224r63_2011$B3_sre, col=clr)
 
+# plot the final band, namely the NIR, band number 4
+
+clnir <- colorRampPalette(c('red','orange','yellow'))(100)
+plot(p224r63_2011$B4_sre, col=clnir)
+
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+
+# how to use band number 4, put the nir infra red in the red component of the rgb, 
+# all the vegetation are now in red, some blank points refers to human infrastructure 
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
