a956af60f8e79a9ae330d39b09ea08b9483bb984
francescovetto <73694014+francescovetto@users.noreply.github.com>
2021-01-11 11:54:14 +0100

    Update R_code_exam_simulation.r

--- R_code_exam_simulation.r
+++ R_code_exam_simulation.r
@@ -3,7 +3,7 @@
 # nc data: we need library to read it ncdf4
 # https://land.copernicus.vgt.vito.be/PDF/portal/Application.html#Home
 # c_gls_LST10-DC_202010010000_GLOBE_GEO_V1.2.1.nc
-# 
+# c_gls_LST10-DC_202101010000_GLOBE_GEO_V1.2.1.nc
 
 install.packages("ncdf4")
 
@@ -16,6 +16,15 @@ tjan <- raster("c_gls_LST10-DC_202010010000_GLOBE_GEO_V1.2.1.nc")
 plot(tjan)
 
 # exercise: change the color ramp palette 
-# cltjan <- colorRampPalette(c('black','grey','light grey'))(100)
+cltjan <- colorRampPalette(c('blue','yellow','red'))(100)
+plot(tjan, col=cltjan)
+dev.off()
 
+toct <- raster("c_gls_LST10-DC_202101010000_GLOBE_GEO_V1.2.1.nc")
+plot(toct, col=cltjan)
 
+dift <- tjan - toct
+cldif <- colorRampPalette(c('blue','white','red'))(100)
+plot(dift, col=cldif)
+
+# difference on the soil temperature 
