9daee5589774675d26946d436dab170510d9a761
francescovetto <73694014+francescovetto@users.noreply.github.com>
2021-01-11 10:22:49 +0100

    Create R_code_exam_simulation.r

---
+++ R_code_exam_simulation.r
@@ -0,0 +1,11 @@
+# R code for the exam 
+
+# nc data: we need library 
+# https://land.copernicus.vgt.vito.be/PDF/portal/Application.html#Home
+# c_gls_LST10-DC_202101010000_GLOBE_GEO_V1.2.1.nc
+
+# install.packages("ncdf4")
+
+library(ncdf4)
+library(raster)
+
