86584f6dd064401e3f575fd32167975808cf5e81
francescovetto <73694014+francescovetto@users.noreply.github.com>
2021-01-18 10:59:50 +0100

    Update R_code_energy.r

--- R_code_energy.r
+++ R_code_energy.r
@@ -41,10 +41,78 @@ italy_ext <- c(0,20,35,55)
 copNDVI_Italy <- crop(copNDVI, italy_ext)
 plot(copNDVI_Italy, col=clymax)
 
+
 ########## deforestation example
+
+
 library(raster)
 library(RStoolbox)
 
+setwd("/Users/francescovettore/Desktop/lab")
 
+# A RasterBrick is a multi-layer raster object. 
+defor1 <- brick("defor1_.jpg.png")  #defor1_.jpg.png
+plotRGB(defor1, 1, 2, 3, stretch="Lin")
 
-defor1 <- brick("defor1_.jpg")  #defor1_.png
+# import image defor2 
+defor2 <- brick("defor2_.jpg.png")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+
+# par can be used to set or query graphical parameters.
+# Three layers are combined such that they represent the red, green and blue channel.
+par(mfrow=c(2,1))
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+
+dev.off()
+# take the image defor1, link the image through the $ to the image of the NIR 
+# and minus the band of the red channel 
+# doing that we calculate the DVI for the 1st image, period
+
+dvi1 <- defor1$defor1_.jpg.1 - defor1$defor1_.jpg.2
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) 
+# specifying a color scheme
+plot(dvi1, col=cl)
+
+# dvi2
+dvi2 <- defor2$defor2_.jpg.1 - defor2$defor2_.jpg.2
+
+# the higher amount of yellow, the lower the capability of the system 
+# to do photosynthesis, to produce oxygen, biomass 
+
+# main, to put a label, a title to the image
+par(mfrow=c(2,1))
+plot(dvi1, col=cl, main="DVI before cut")
+plot(dvi2, col=cl, main="DVI after cut")
+
+# dvi1 biomass before cut, dvi2 biomass after cut 
+# the difference between 1 and 2
+# if biomass was the same we'd have no difference
+difdvi <- dvi1 - dvi2
+# warning, images have a small difference in their extent 
+dev.off()
+plot(difdvi)
+
+cldif <- colorRampPalette(c('blue','white','red'))(100)
+plot(difdvi, col=cldif)
+
+cldif <- colorRampPalette(c('blue','white','red'))(100)
+plot(difdvi, col=cldif, main="amount of energy lost!")
+
+# histogram
+# The generic function hist computes a histogram of the given data values
+hist(difdvi, col="red")
+
+par(mfrow=c(3,2))
+# defor1 
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+# defor2
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+# dvi1
+plot(dvi1, col=cl, main="biomass before cut")
+# dvi2
+plot(dvi2, col=cl, main="biomass after cut")
+# difdvi
+plot(difdvi, col=cldif, main="amount of energy lost!")
+# histogram
+hist(difdvi, col="red")
