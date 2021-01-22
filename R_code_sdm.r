e8c4e0fb8bf69a05ae1424672ccef41715237854
francescovetto <73694014+francescovetto@users.noreply.github.com>
2021-01-22 10:31:46 +0100

    Create R_code_sdm.r

---
+++ R_code_sdm.r
@@ -0,0 +1,51 @@
+# R code sdm
+
+install.packages("sdm")
+library(sdm)
+library(raster)
+library(rgdal)
+
+# import species data which are in the sdm package 
+
+file <- system.file("external/species.shp", package="sdm")
+species <- shapefile(file) # readOGR to ext species 
+species
+
+plot(species, pch=17)
+# we should subdivide the points with presence and thos with absence 
+
+species$Occurrence
+plot(species[species$Occurrence == 1,], col='blue', pch=17)
+# points to add points to the previous plot 
+points(species[species$Occurrence == 0,], col='red', pch=17)
+
+# predictors: all of 4 variables elevation, precipitation, temperature, biomass
+path <- system.file("external", package="sdm")
+# list od the predictors 
+lst <- list.files(path=path,pattern='asc$',full.names = T) # ascii extension
+lst
+
+preds <- stack(lst)
+# we can plot the data all together since they are in a stack
+cl <- colorRampPalette(c('blue','orange','red','yellow')) (100)
+plot(preds, col=cl)
+
+plot(preds$elevation, col=cl, main='elevation')
+points(species[species$Occurrence == 1,],pch=17)
+
+plot(preds$precipitation, col=cl, main='precipitation')
+points(species[species$Occurrence == 1,],pch=17)
+
+plot(preds$temperature, col=cl, main='temperature')
+points(species[species$Occurrence == 1,],pch=17)
+
+plot(preds$vegetation, col=cl, main='vegetation')
+points(species[species$Occurrence == 1,],pch=17)
+
+###### model 
+
+# set the data for the sdm 
+datasdm <- sdmData(train=species, predictors=preds)
+datasdm
+
+m1 <- sdm(Occurrence )
